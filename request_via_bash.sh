11f309e645fdfe978d74ae571b8e0fa714c1f903
Dan Sun <dansun47@gmail.com>
2020-04-30 17:34:17 -0400

    modified URL path

--- request_via_bash.sh
+++ request_via_bash.sh
@@ -1,3 +1,3 @@
 curl -i -X POST --header "Content-Type: image/jpeg" \
     --data-binary "@/home/dansun47/Elastic-Inference/test_data/img_1.jpg" \
-    http://localhost:8080/api/test
+    http://localhost:8080/mnist
